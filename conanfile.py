a7317f1b5bfb2a18e18249e7e33d5bd7e85b298f
Florian Berchtold <florian.berchtold@luminartech.com>
2020-11-29 22:37:56 +0100

    oldschoold toolchain file + ccache

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,8 @@ from conans import ConanFile, CMake, CMakeToolchain
 
 class modern_cmake(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = ("catch2/2.13.3",)
+    requires = (
+        "catch2/2.13.3",
+        "opencv/4.5.0",
+    )
     generators = "cmake_find_package"
-
-    def toolchain(self):
-        tc = CMakeToolchain(self)
-        tc.write_toolchain_files()
